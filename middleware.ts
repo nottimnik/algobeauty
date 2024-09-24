bab8e5ffa234cd8c7d2964ea650297008ce138e1
timnikofficial <timnikofficial@gmail.com>
2024-09-24 09:56:32 +0000

    dashboard protection

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,77 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
 
-export default async function middleware(request: NextRequest) {
-  const response = await updateSession(request)
+export async function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
 
-  // Ensure a valid response is returned
-  return response || NextResponse.next()
+	let response = NextResponse.next({
+		request: {
+			headers: request.headers,
+		},
+	});
+
+	const supabase = createServerClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+		{
+			cookies: {
+				get(name: string) {
+					return request.cookies.get(name)?.value;
+				},
+				set(name: string, value: string, options: CookieOptions) {
+					request.cookies.set({
+						name,
+						value,
+						...options,
+					});
+					response = NextResponse.next({
+						request: {
+							headers: request.headers,
+						},
+					});
+					response.cookies.set({
+						name,
+						value,
+						...options,
+					});
+				},
+				remove(name: string, options: CookieOptions) {
+					request.cookies.set({
+						name,
+						value: "",
+						...options,
+					});
+					response = NextResponse.next({
+						request: {
+							headers: request.headers,
+						},
+					});
+					response.cookies.set({
+						name,
+						value: "",
+						...options,
+					});
+				},
+			},
+		}
+	);
+
+	await supabase.auth.getSession();
+
+	const { data } = await supabase.auth.getSession();
+
+	if (data.session) {
+		if (
+			// protect this page only admin can access this /dashboard/members
+			data.session.user.user_metadata.role !== "admin"
+		) {
+			return NextResponse.redirect(new URL("/", request.url));
+		}
+	} else {
+		return NextResponse.redirect(new URL("/", request.url));
+	}
 }
 
 export const config = {
-  matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
-}
+	matcher: ["/dashboard/:path*"],
+};
