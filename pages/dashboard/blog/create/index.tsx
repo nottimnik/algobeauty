e20a537de9e096fb7c242ef9f1da0b99a26865fc
timnikofficial <timnikofficial@gmail.com>
2024-09-25 12:53:17 +0000

    dashboard 1.25.02

--- pages/dashboard/blog/create/index.tsx
+++ pages/dashboard/blog/create/index.tsx
@@ -24,20 +24,33 @@ import {
 } from "@radix-ui/react-icons";
 import { Switch } from "@/components/ui/switch";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const FormSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
   }),
+  image_url: z.string().url({ message: "Invalid url" }),
+  content: z.string().min(2, {
+    message: "Content must be at least 2 characters.",
+  }),
+  is_published: z.boolean(),
+  is_premium: z.boolean(),
 });
 
 export default function BlogForm() {
   const [isPreview, setIsPreview] = useState(false);
 
   const form = useForm<z.infer<typeof FormSchema>>({
+    mode: "all",
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      username: "",
+      title: "",
+      content: "",
+      image_url: "",
+      is_premium: false,
+      is_published: true,
     },
   });
 
@@ -58,7 +71,7 @@ export default function BlogForm() {
         onSubmit={form.handleSubmit(onSubmit)}
         className="w-full border rounded-md space-y-6"
       >
-        <div className="p-5 flex items-center flex-wrap justify-between border-b">
+        <div className="p-5 flex items-center flex-wrap justify-between border-b gap-5">
           <div className="flex gap-5 items-center">
             <span
               role="button"
@@ -82,14 +95,17 @@ export default function BlogForm() {
             </span>
             <FormField
               control={form.control}
-              name="username"
+              name="is_premium"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
                     <div className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-md">
                       <StarIcon />
                       <span>Premium</span>
-                      <Switch />
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
                     </div>
                   </FormControl>
                 </FormItem>
@@ -97,45 +113,125 @@ export default function BlogForm() {
             />
             <FormField
               control={form.control}
-              name="username"
+              name="is_published"
               render={({ field }) => (
                 <FormItem>
                   <FormControl>
                     <div className="flex items-center gap-1 border bg-zinc-700 p-2 rounded-md">
                       <RocketIcon />
                       <span>Publish</span>
-                      <Switch />
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
                     </div>
                   </FormControl>
                 </FormItem>
               )}
             />
           </div>
-          <Button type="submit" className="flex items-center gap-1">
+          <Button
+            type="submit"
+            className="flex items-center gap-1"
+            disabled={!form.formState.isValid}
+          >
             <BsSave />
             Save
           </Button>
         </div>
         <FormField
           control={form.control}
-          name="username"
+          name="title"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <div
+                  className={cn(
+                    "p-2 w-full flex break-words gap-2",
+                    isPreview ? "divide-x-0" : "divide-x"
+                  )}
+                >
+                  <Input
+                    placeholder="Title"
+                    {...field}
+                    className={cn(
+                      "border-none text-lg font-medium leading-relaxed",
+                      isPreview ? "w-0 p-0" : "w-full lg:w-1/2"
+                    )}
+                  />
+                  <div
+                    className={cn(
+                      "lg:px-10",
+                      isPreview
+                        ? "mx-auto w-full lg:w-4/5"
+                        : "w-1/2 lg:block hidden"
+                    )}
+                  >
+                    <h1 className="text-3xl font-medium">
+                      {form.getValues().title}
+                    </h1>
+                  </div>
+                </div>
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+              {form.getFieldState("title").invalid &&
+                form.getValues().title && <FormMessage />}
             </FormItem>
           )}
         />
-        <Button type="submit" className="flex items-center gap-1">
-          <BsSave />
-          Submit
-        </Button>
+        <FormField
+          control={form.control}
+          name="image_url"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div
+                  className={cn(
+                    "p-2 w-full flex break-words gap-2",
+                    isPreview ? "divide-x-0" : "divide-x"
+                  )}
+                >
+                  <Input
+                    placeholder="Image Url"
+                    {...field}
+                    className={cn(
+                      "border-none text-lg font-medium leading-relaxed",
+                      isPreview ? "w-0 p-0" : "w-full lg:w-1/2"
+                    )}
+                  />
+                  <div
+                    className={cn(
+                      "lg:px-10",
+                      isPreview
+                        ? "mx-auto w-full lg:w-4/5"
+                        : "w-1/2 lg:block hidden"
+                    )}
+                  >
+                    {!isPreview ? (
+                      <>
+                        <p>Click on preview to see image</p>
+                      </>
+                    ) : (
+                      <div className="relative">
+                        <Image
+                          src={form.getValues().image_url}
+                          alt="preview"
+                          fill
+                          className="object-cover object-center"
+                        />
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </FormControl>
+              {form.getFieldState("image_url").invalid &&
+                form.getValues().image_url && (
+                  <div className="p-2">
+                    <FormMessage />
+                  </div>
+                )}
+            </FormItem>
+          )}
+        />
       </form>
     </Form>
   );
