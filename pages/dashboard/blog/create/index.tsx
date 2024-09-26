7b00290e920e8f78f10b38dce4ca71c775c2c4ed
timnikofficial <timnikofficial@gmail.com>
2024-09-26 15:18:30 +0000

    commented code to it doesn't give error

--- pages/dashboard/blog/create/index.tsx
+++ pages/dashboard/blog/create/index.tsx
@@ -8,23 +8,22 @@ import { createBlog } from "@/actions/blog";
 
 export default function index() {
   const handleCreate = async (data: BlogFormSchemaType) => {
-    const result = await createBlog(data);
-    const { error } = JSON.parse(result);
-
-    if (error?.message) {
-      toast({
-        title: "Failed! Error:",
-        description: (
-          <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
-            <code className="text-white">{error.message}</code>
-          </pre>
-        ),
-      });
-    } else {
-      toast({
-        title: "Succesfully created the blog post: '" + data.title + "'",
-      });
-    }
+      // const result = await createBlog(data); // problem with this one: can't resolve '@vercel/turbopack-ecmascript-runtime/dev/client/hmr-client.ts'
+    //   const { error } = JSON.parse(result);
+    // if (error?.message) {
+    //   toast({
+    //     title: "Failed! Error:",
+    //     description: (
+    //       <pre className="mt-2 w-full rounded-md bg-slate-950 p-4">
+    //         <code className="text-white">{error.message}</code>
+    //       </pre>
+    //     ),
+    //   });
+    // } else {
+    //   toast({
+    //     title: "Succesfully created the blog post: '" + data.title + "'",
+    //   });
+    // }
   };
 
   return <BlogForm onHandleSubmit={handleCreate} />;
